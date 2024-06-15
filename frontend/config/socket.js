791f230d0ef3518c6b2aee048c2eb6d6a39f2ab0
Rahul Nain <connecttorahulnain@gmail.com>
2024-06-15 21:41:12 +0530

    added scripts

--- frontend/config/socket.js
+++ frontend/config/socket.js
@@ -1,7 +1,7 @@
 import {io,Socket} from 'socket.io-client';
 
 
-const ENDPOINT = 'https://whiteboard-ccmjnq2tl-rahul-nain172s-projects.vercel.app';
+const ENDPOINT = 'https://whiteboard-3hy1nqaib-rahul-nain172s-projects.vercel.app/';
 const socket=io(ENDPOINT,{
     withCredentials: true,
     transports: ['websocket','polling'],
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,15 +6,12 @@ import path from 'path';
 export default defineConfig({
   plugins: [react()],
   server: {
-    https: {
-      key: fs.readFileSync(path.resolve(__dirname, 'localhost-key.pem')),
-      cert: fs.readFileSync(path.resolve(__dirname, 'localhost-cert.pem')),
-    },
     port: 5173, // The port your Vite dev server is running on
-    hmr: {
-      protocol: 'wss',
-      host: 'localhost',
-      port: 5173,
-    },
+    server: {
+      hmr: {
+          host: "localhost",
+          protocol: "ws",
+      },
+    }
   },
 });
