d11aa7c74c5235c2ff29a724de3d16deb726cbd7
Rahul Nain <connecttorahulnain@gmail.com>
2024-06-15 19:33:47 +0530

    assigned correct url to socket io

--- frontend/config/socket.js
+++ frontend/config/socket.js
@@ -1,6 +1,6 @@
 import {io,Socket} from 'socket.io-client';
 
-const socket=io('http://localhost:3000/');
+const socket=io('https://whiteboard-dhhob5omn-rahul-nain172s-projects.vercel.app/');
 socket.on('connect', () => {
 });
 export default socket;
